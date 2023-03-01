86a7a5ded79ec99be488fbfc902f8cdc4245b058
sugiartofahmi <sugiartofahmi@gmail.com>
2023-03-01 15:36:20 +0700

    Fahmi Sugiarto | Add Env

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { lazy, Suspense } from "react";
-import Navbar from "./components/Navbar";
+
 import Router from "./router";
 const App = () => {
   return (
     <Suspense fallback="Loading...">
-      <Navbar />
       <Suspense fallback="Loading...">
         <Router />
       </Suspense>
